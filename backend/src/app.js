ab3abca3d7c3cd188d937e45f21c4ba24a3b6ca1
onurekenn91 <senin-github-emailin@mail.com>
2026-04-04 19:11:33 +0300

    Fix backend Mongo connection on requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,11 +6,22 @@ const matchRoutes = require("./routes/matchRoutes");
 const teamRoutes = require("./routes/teamRoutes");
 const playerRoutes = require("./routes/playerRoutes");
 const predictionRoutes = require("./routes/predictionRoutes");
+const { ensureDb } = require("./utils/dbRuntime");
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
 app.use(express.json());
+app.use((req, res, next) => {
+  ensureDb()
+    .then(() => next())
+    .catch((error) => {
+      res.status(503).json({
+        message: "Veritabani baglantisi kurulamadi",
+        detail: error.message,
+      });
+    });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Goalio API aktif" });
@@ -21,5 +32,10 @@ app.use("/matches", matchRoutes);
 app.use("/teams", teamRoutes);
 app.use("/players", playerRoutes);
 app.use("/predictions", predictionRoutes);
+
+app.use((error, req, res, next) => {
+  console.error("Beklenmeyen sunucu hatasi:", error);
+  res.status(500).json({ message: "Beklenmeyen sunucu hatasi" });
+});
 
 module.exports = app;
